922704732ea5413a341e4d88a62ea555aff57983
Elliot Tam <eteaisme@gmail.com>
2024-09-26 07:34:45 -0400

    Added PID controllers

--- src/main/java/frc/robot/subsystems/Drivetrain/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/Drivetrain/SwerveModule.java
@@ -1,8 +1,7 @@
 package frc.robot.subsystems.Drivetrain;
 
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
 /*This module should contain:
@@ -14,23 +13,38 @@ public class SwerveModule {
     //Define all the varibles
     private Talon driveMotor; 
     private Talon turningMotor; 
-    private SwerveModuleState currentState; 
 
+    //State Varibles
+    private SwerveModuleState currentState; 
+    private SwerveModuleState desiredState;
+
+    //Two PID controllers (1 for drive 1 for steering)
+    PIDController drivePIDController; 
+    PIDController turningPIDController;
+
+    //Constructor 
     SwerveModule(
         int driveMotorPort,
         int turningMotorPort        
     ) {
         System.out.println("SwerveModule Constructor");
+
+
         driveMotor = new Talon(driveMotorPort);
         turningMotor = new Talon(turningMotorPort);
-        currentState = new SwerveModuleState(); 
+
+        drivePIDController = new PIDController(0.1, 0, 0 ); 
+        turningPIDController = new PIDController(0.1, 0, 0); 
+
+        currentState = new SwerveModuleState();
+        
    } 
 
    public SwerveModuleState getState() {
        return currentState;
   }
 
-  public void setState(SwerveModuleState newState) {
-      currentState = newState; 
+  public void setDesiredState(SwerveModuleState newState) {
+    desiredState = newState; 
   }
 }
--- src/main/java/frc/robot/subsystems/Drivetrain/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/Drivetrain/SwerveSubsystem.java
@@ -1,16 +1,18 @@
 package frc.robot.subsystems.Drivetrain;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class SwerveSubsystem extends SubsystemBase {
+    PIDController pid = new PIDController(0, 0, 0); 
+
     //Drive train dimensions 
     public static final double DRIVE_BASE_X = Units.inchesToMeters(24.750);
     public static final double DRIVE_BASE_Y = Units.inchesToMeters(24.750);
@@ -36,10 +38,10 @@ public class SwerveSubsystem extends SubsystemBase {
     public void setChasisSpeeds(ChassisSpeeds desiredSpeed){
         SwerveModuleState[] newStates = kinematics.toSwerveModuleStates(desiredSpeed);
         
-        frontLeftModule.setState(newStates[0]);
-        frontRightModule.setState(newStates[1]);
-        backLeftModule.setState(newStates[2]);
-        backRightModule.setState(newStates[3]);
+        frontLeftModule.setDesiredState(newStates[0]);
+        frontRightModule.setDesiredState(newStates[1]);
+        backLeftModule.setDesiredState(newStates[2]);
+        backRightModule.setDesiredState(newStates[3]);
     }
     @Override
     public void periodic() {
